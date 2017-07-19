e175c9ace10ccbbb302b009ec547bd7529b612b4
zhengyao <yao.zheng@biz-united.com.cn>
2017-07-19 17:21:58 +0800

    rm app

--- app/src/main/java/com/warehourse/app/event/LoginEvent.java
+++
@@ -1,7 +0,0 @@
-package com.warehourse.app.event;
-
-/**
- * Created by johnzheng on 3/23/16.
- */
-public class LoginEvent {
-}
--- app/src/main/java/com/warehourse/app/event/ShareEvent.java
+++
@@ -1,9 +0,0 @@
-package com.warehourse.app.event;
-
-/**
- * Created by johnzheng on 3/22/16.
- */
-public class ShareEvent {
-
-
-}
