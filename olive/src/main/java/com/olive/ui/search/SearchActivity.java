3f7b6b64c2aa9fc2c6ea524907edf700dddb44b7
zhengyao <yao.zheng@biz-united.com.cn>
2017-07-20 14:12:24 +0800

    fixed home

---
+++ olive/src/main/java/com/olive/ui/search/SearchActivity.java
@@ -0,0 +1,25 @@
+package com.olive.ui.search;
+
+import com.biz.base.BaseActivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Title: SearchActivity
+ * Description:
+ * Copyright:Copyright(c)2016
+ * Company:博智维讯信息技术有限公司
+ * CreateTime:20/07/2017  14:00
+ *
+ * @author johnzheng
+ * @version 1.0
+ */
+
+public class SearchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
